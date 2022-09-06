0d4c503ea750f3a77bdc4bbc9f9c6180cbaf92cb
RewEPIC <78597357+RewEPIC@users.noreply.github.com>
2022-09-06 19:54:22 +0700

    Update minMedMax.js

--- minMedMax.js
+++ minMedMax.js
@@ -2,13 +2,8 @@ const { template } = require('@babel/core')
 
 function minMedMax(n1, n2, n3) {
     let arrays = [n1,n2,n3]
-    //Limit 3 arrays sort only.
-    for (let i = 0;i < arrays.length;i++) {
-        if (arrays[i] > arrays[i+1]) {
-            swap(arrays,i,i+1)
-            if (arrays[i-1] > arrays[i]) swap(arrays,i-1,i)
-        }
-    }
+    sort(arrays,0,1)
+    if (sort(arrays,1,2)) sort(arrays,0,1)
     return { min:arrays[0] ,mid:arrays[1], max:arrays[2] }
 }
 function swap(arr,n1,n2) {
@@ -16,5 +11,12 @@ function swap(arr,n1,n2) {
     arr[n1] = arr[n2]
     arr[n2] = temp
     return arr
+}
+function sort(arr,n1,n2) {
+    if (arr[n1] > arr[n2]) {
+        swap(arr,n1,n2)
+        return true;
+    }
+    return false;
 }
 module.exports = minMedMax
